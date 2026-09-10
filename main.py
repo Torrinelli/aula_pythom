fe65bc1a5accdcb77bf5d305ca593f7f03373ccb
Frederico Torrinelli <torrinelli@gmail.com>
2026-09-10 10:49:37 -0300

    crating main loop connecting Menu and Gamer classes

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import pygame
+from code.Game import Game
 
-print('Setup Start')
-pygame.init()
-window = pygame.display.set_mode((600, 480))
-print('Setup Finish')
-
-print('Loop start')
-while True:
-    # Check for all events (precisa ficar indentado aqui dentro)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
+game = Game()
+game.run()
+pygame.quit()
